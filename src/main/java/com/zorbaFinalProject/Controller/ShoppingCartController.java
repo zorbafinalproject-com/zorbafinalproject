03d6f511f48eaee78c0fbf4e8b53b150bdb0ef21
rajesh2shrestha <rajesh2shrestha@gmail.com>
2022-05-25 00:51:48 -0400

    Controller added

---
+++ src/main/java/com/zorbaFinalProject/Controller/ShoppingCartController.java
@@ -0,0 +1,10 @@
+package com.zorbaFinalProject.Controller;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ShoppingCartController {
+}
