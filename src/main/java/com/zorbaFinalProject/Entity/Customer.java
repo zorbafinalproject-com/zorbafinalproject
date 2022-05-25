ba3039dee7d8e32d64b2f52ec062e46511db5734
rajesh2shrestha <rajesh2shrestha@gmail.com>
2022-05-25 01:32:44 -0400

    Added spring validation

--- src/main/java/com/zorbaFinalProject/Entity/Customer.java
+++ src/main/java/com/zorbaFinalProject/Entity/Customer.java
@@ -2,9 +2,12 @@ package com.zorbaFinalProject.Entity;
 
 import com.sun.istack.NotNull;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "Customer")
@@ -12,6 +15,10 @@ public class Customer {
 
     @Id
     @NotNull
+    @NotBlank
     private int custId;
+
+    @Column(name = "custName")
+    private String custName;
 
 }
