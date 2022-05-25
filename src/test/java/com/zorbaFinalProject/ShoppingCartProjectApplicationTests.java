a77162f14065ab7e2e92c2e814a89e6151891f63
rajesh2shrestha <rajesh2shrestha@gmail.com>
2022-05-24 23:48:55 -0400

    new spring project

---
+++ src/test/java/com/zorbaFinalProject/ShoppingCartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorbaFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
