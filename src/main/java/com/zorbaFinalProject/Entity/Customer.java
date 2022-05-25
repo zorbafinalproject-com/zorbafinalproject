3ec0822b2ac1e4ddc32ca906239ec06f4364b6f6
rajesh2shrestha <rajesh2shrestha@gmail.com>
2022-05-25 01:26:40 -0400

    added ID

--- src/main/java/com/zorbaFinalProject/Entity/Customer.java
+++ src/main/java/com/zorbaFinalProject/Entity/Customer.java
@@ -1,12 +1,17 @@
 package com.zorbaFinalProject.Entity;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Customer")
 public class Customer {
 
+    @Id
+    @NotNull
     private int custId;
 
 }
